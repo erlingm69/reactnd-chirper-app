a0e2c510b5f381282296b478d432f3048f94347f
Erling Mårtensson <erling.martensson@gmail.com>
2021-05-31 14:34:52 +0200

    test: Call handleInitialData

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,8 @@
 export const SET_AUTH_USER = "SET_AUTH_USER"
 
-export function receiveTweets(tweets) {
+export function setAuthedUser(id) {
     return {
         type: SET_AUTH_USER,
-        id: "tylermcginnis"
+        id
     }
 }
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,12 +3,15 @@ import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
 import { setAuthedUser } from '../actions/authedUser'
 
-export function handleInitialData() {
-    return (dispatch) => {
-        return getInitialData().then(({ users, tweets }) => {
-            dispatch(receiveUsers(users))
-            dispatch(receiveTweets(tweets)),
-            dispatch(setAuthedUser())
-        })
-    }
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, tweets }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveTweets(tweets))
+        dispatch(setAuthedUser(AUTHED_ID))
+      })
+  }
 }
