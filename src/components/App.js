29897a2bfccec25cfecb6554cbe96f46c733fbf6
Erling Mårtensson <erling.martensson@gmail.com>
2021-06-01 10:54:59 +0200

    test: Conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Dashboard />
+        {
+          this.props.loading ? null : <Dashboard />
+        }
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
